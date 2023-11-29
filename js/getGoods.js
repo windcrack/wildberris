3e692613146b5a39fc5c50ebea330a71d8d29583
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2023-11-29 21:57:11 +0300

    lesson 3

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,27 +1,64 @@
 const getGoods = () =>{
     const links = document.querySelectorAll('.navigation-link');
+    const linkViewAll = document.querySelector('.more');
 
-    const getData = () =>{
+    const renderGoods = (goods) =>{
+        const goodsContainer = document.querySelector('.long-goods-list');
+        goodsContainer.innerHTML = "";
+        goods.forEach(good =>{
+            const goodBlock = document.createElement('div');
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+
+            goodBlock.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                <h3 class="goods-title">${good.name}</h3>
+                <p class="goods-description">${good.description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                    <span class="button-price">$${good.price}</span>
+                </button>
+            </div>
+            
+            `;
+
+            goodsContainer.append(goodBlock);
+        })
+    };
+
+    const getData = (value, category) =>{
         fetch('https://wb-pet-default-rtdb.asia-southeast1.firebasedatabase.app/db.json')
         .then(res => res.json())
-        .then(data => localStorage.setItem('data', JSON.stringify(data)));
+        .then(data => {
+            const arr = category ? data.filter((element) =>  element[category] === value) : data;
+            localStorage.setItem('goods', JSON.stringify(arr))
+            if(window.location.pathname !== '/goods.html'){
+                window.location.href = '/goods.html';
+            }
+            renderGoods(arr);
+        });
     }
-
-    getData();
 
     links.forEach(link =>{
         link.addEventListener('click', (event) =>{
             event.preventDefault();
-            getData();
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+            getData(linkValue, category);
         })
     });
 
-    localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]));
-    const goods = JSON.parse(localStorage.getItem('goods'));
-    const data = JSON.parse(localStorage.getItem('data'));
-    console.log([goods, data]);
-    localStorage.removeItem('goods');
-    // console.log(localStorage);
+    if(localStorage.getItem('goods') && window.location.pathname === '/goods.html'){
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
+    
+    if(!!linkViewAll){
+        linkViewAll.addEventListener('click', (e) =>{
+            e.preventDefault();
+            getData();
+        });
+    }
 }
 
 getGoods();
--- js/search.js
+++ js/search.js
@@ -2,13 +2,50 @@ const search = function(){
     const input = document.querySelector('.search-block > input');
     const searchBtn = document.querySelector('.search-block > button');
 
-    // input.addEventListener('input', (event) =>{
-    //     console.log('input', event.target.value);
-    // });
-    searchBtn.addEventListener('click', () =>{
-        console.log(input.value);
-    });
+    const renderGoods = (goods) =>{
+        const goodsContainer = document.querySelector('.long-goods-list');
+        goodsContainer.innerHTML = "";
+        goods.forEach(good =>{
+            const goodBlock = document.createElement('div');
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
 
+            goodBlock.innerHTML = `
+            <div class="goods-card">
+                <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                <h3 class="goods-title">${good.name}</h3>
+                <p class="goods-description">${good.description}</p>
+                <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                    <span class="button-price">$${good.price}</span>
+                </button>
+            </div>
+            
+            `;
+
+            goodsContainer.append(goodBlock);
+        })
+    };
+
+    const getData = (value) =>{
+        fetch('https://wb-pet-default-rtdb.asia-southeast1.firebasedatabase.app/db.json')
+        .then(res => res.json())
+        .then(data => {
+            const arr = data.filter(good => good.name.toLowerCase().includes(value.toLowerCase()));
+
+            localStorage.setItem('goods', JSON.stringify(arr))
+            if(window.location.pathname !== '/goods.html'){
+                window.location.href = '/goods.html';
+            }
+            renderGoods(arr);
+            //window.location.href = '/goods.html';
+        });
+    }
+
+    searchBtn.addEventListener('click', () =>{
+        getData(input.value);
+    });
+    
 }
 
 search();
